5ae6b42bd532373243b55e2dff21f56d6b9f68de
Kyle Krzeski <krzeski@vt.edu>
2016-03-19 14:03:29 +0000

    added icon to title and also validations to models

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -1,6 +1,7 @@
 class TodoItem < ActiveRecord::Base
   belongs_to :todo_list
   
+  validates_length_of :content, :maximum => 160
   #methods for our model
   
   #if completed it means that the completed_at field is not blank
--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,3 +1,7 @@
 class TodoList < ActiveRecord::Base
   has_many :todo_items
+  
+  validates_length_of :description, :maximum => 160
+  validates_length_of :title, :maximum => 60
+  
 end
